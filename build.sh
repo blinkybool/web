1aa8d227f3ef64a1f3944fd8984f33b89eac0da9
Billy Snikkers <billyprice98is@gmail.com>
2023-06-24 20:43:59 +0200

    fixed build.sh removing

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 bundle exec jekyll build --destination gh-pages/docs
 touch gh-pages/docs/.nojekyll
 cp CNAME gh-pages/docs
-rm gh-pages/build.sh
+rm gh-pages/docs/build.sh
 cd gh-pages
 git add -A
 git commit -m "build"
